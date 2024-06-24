6518de3940023550dccc7cdee30e6af770c4bb99
vasilecuzmin <vasile.cuzmin@simpology.com>
2024-06-24 15:46:24 +0300

    sfd

---
+++ EFCorePractice/AdventureWorks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
